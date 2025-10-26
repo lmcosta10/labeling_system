031c32f554211ad9dcb1954865694f0c1902703f
lmcosta10 <letmc14@gmail.com>
2025-10-26 17:30:41 -0300

    feat(frontend): create tags request

--- front/src/components/Image.tsx
+++ front/src/components/Image.tsx
@@ -15,21 +15,33 @@ export default function ImageDetails({ selectedImage, onImageClick } : ImageDeta
 
     const [tags, setTags] = useState<string[]>([]);
 
-    useEffect(() => {
-        async function fetchImageTags() {
-            // TODO: remove: MOCK TAGS
-            setTags(["tag1", "tag2"]);
+    const API_BASE = import.meta.env.VITE_API_URL;
+    const imageUrlStart = `${API_BASE}/api/image`;
 
-            const API_BASE = import.meta.env.VITE_API_URL;
-            try {
-            const response = await fetch(`${API_BASE}/api/image/:id`);
-            if (!response.ok) throw new Error("Failed to fetch image");
-            } catch (err) {
-                console.error(err);
-            }
+    useEffect(() => {
+      async function fetchImageTags() {
+        try {
+          const imageUrl = `${imageUrlStart}/${selectedImage.id}`;
+
+          const response = await fetch(imageUrl);
+
+          if (!response.ok) throw new Error("Failed to fetch image");
+
+          const data = await response.json();
+
+          if (data.success && typeof data.tags === "string") {
+            const parsedTags = data.tags.split(";").map((tag: string) => tag.trim()).filter(Boolean); // TODO
+            setTags(parsedTags);
+          } else {
+            console.warn("Unexpected data format:", data);
+            setTags([]);
+          }
+        } catch (err) {
+            console.error(err);
         }
+      }
     fetchImageTags();
-    }, [tags]);
+    }, [imageUrlStart, selectedImage.id]);
     
     return(
     <div>
@@ -46,11 +58,11 @@ export default function ImageDetails({ selectedImage, onImageClick } : ImageDeta
             {tags.length > 0 ? (
               tags.map((tag) => (
                 <span>
-                  {tag}
+                  {tag}<p></p>
                 </span>
               ))
             ) : (
-              <p>Nenhuma tag adicionada ainda.</p>
+              <p>No tags yet.</p>
             )}
         </div>
     </div>
