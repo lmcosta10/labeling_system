285b10f5c7ab38007f00ead2282599daa5a6dc07
lmcosta10 <letmc14@gmail.com>
2025-12-10 10:55:15 -0300

    feat(backend): send user groups response grouped by user group

--- server/src/user_groups/repository.rs
+++ server/src/user_groups/repository.rs
@@ -2,15 +2,17 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct UserGroupsResponse {
-    username: String,
-    group: u32
+    group: u32,
+    usernames: Vec<String>
 }
 
 pub fn get_all_user_groups() -> Result<Vec<UserGroupsResponse>, anyhow::Error> {
     let conn = sqlite::open("./src/database/labelsys.db")?; // drop method is called implicitly
 
-    let user_groups_query = "SELECT * FROM user_groups";
+    let user_groups_query = "SELECT * FROM user_groups ORDER BY \"group\""; // "ORDER BY": TODO: optimize code later
     let mut user_groups_statement = conn.prepare(user_groups_query)?;
+
+    let mut all_groups: Vec<u32> = Vec::new();
 
     let mut found_user_groups: Vec<UserGroupsResponse> = Vec::new();
     
@@ -19,12 +21,28 @@ pub fn get_all_user_groups() -> Result<Vec<UserGroupsResponse>, anyhow::Error> {
         let group_i64: i64 = user_groups_statement.read(1)?;
         let group = group_i64 as u32;
 
-        found_user_groups.push(
-            UserGroupsResponse{
-                username: username,
-                group: group
+        // If it's the first user found in a group:
+        if !all_groups.contains(&group) { 
+            let mut usernames: Vec<String> = Vec::new();
+            usernames.push(username);
+
+            found_user_groups.push(
+                UserGroupsResponse{
+                    group: group,
+                    usernames: usernames
+                }
+            );
+            all_groups.push(group); // add group to all_groups (currently found groups in query)
+        } // If the group had already been added:
+        else {
+            all_groups.push(group);
+
+            for found_group in found_user_groups.iter_mut() {
+                if found_group.group == group {
+                    found_group.usernames.push(username.clone());
+                }
             }
-        );
+        };
     }
 
     Ok(found_user_groups)
